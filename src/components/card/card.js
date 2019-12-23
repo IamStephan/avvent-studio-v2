149a2e9491d634d3b468e8a12da572c39a22bea6
I_Am_Stephan <stephanburger54@gmail.com>
2019-12-23 21:20:22 +0200

    added card component, added to the home page

---
+++ src/components/card/card.js
@@ -0,0 +1,85 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './card.module.scss';
+
+class CardMedia extends PureComponent {
+  static propTypes = {
+    height: PropTypes.number,
+    mode: PropTypes.oneOf(['contain', 'cover'])
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: `${this.props.height}px`
+        }}
+        className={`${styles['card-media']} ${styles[this.props.mode]}`}
+      >
+        <img alt={this.props.alt} src={this.props.src} />
+      </div>
+    )
+  }
+}
+
+class CardActions extends PureComponent {
+  static propTypes = {
+    align: PropTypes.oneOf(['left', 'center', 'right'])
+  }
+
+  static defaultProps = {
+    align: 'center'
+  }
+
+  render() {
+    return (
+      <div className={`${styles['card-actions']} ${styles[this.props.align]}`}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class CardContent extends PureComponent {
+  render() {
+    return (
+      <div className={styles['card-content']}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class CardTitle extends PureComponent {
+  render() {
+    return (
+      <h2 className={styles['card-title']}>
+        {this.props.children}
+      </h2>
+    )
+  }
+}
+
+export default class Card extends PureComponent {
+  static propTypes = {
+    mode: PropTypes.oneOf(['light', 'primary'])
+  }
+
+  static defaultProps = {
+    mode: 'light'
+  }
+
+  static Media = CardMedia
+  static Title = CardTitle
+  static Content = CardContent
+  static Actions = CardActions
+
+  render() {
+    return (
+      <div className={`${styles['card']} ${styles[this.props.mode]} ${this.props.className}`}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 import globals from '../../utils/globals.scss';
 import styles from './home.module.scss';
 
+import InfoIcon from '@material-ui/icons/InfoOutlined';
 import Page from '../../components/pageTemplate/pageTemplate';
 import Button from '../../components/button/button';
+import Card from '../../components/card/card';
 
 export default class Home extends Component {
   render() {
@@ -48,6 +50,72 @@ export default class Home extends Component {
 
         <Page.Section navbarMode='primary'>
           <Page.SectionTitle>What we do.</Page.SectionTitle>
+
+          <div className={styles['card-container']}>
+            <div className={styles['cards']}>
+              <Card>
+                <Card.Media alt='E-commerce' src={require('../../static/illustrations/ecommerce_1.svg')} mode='contain' height={200} />
+
+                <Card.Title>
+                  We develop E-commerce sites.
+                </Card.Title>
+
+                <Card.Content>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus.
+                </Card.Content>
+
+                <Card.Actions>
+                  <Button>
+                    Learn More
+                    <InfoIcon style={{
+                      marginLeft: `${globals.margin}px`
+                    }} />
+                  </Button>
+                </Card.Actions>
+              </Card>
+
+              <Card className={styles['second']}>
+                <Card.Media alt='Web' src={require('../../static/illustrations/web_1.svg')} mode='contain' height={200} />
+                <Card.Title>
+                  We develop websites and web apps.
+                </Card.Title>
+
+                <Card.Content>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus.
+                </Card.Content>
+
+                <Card.Actions>
+                  <Button>
+                    Learn More
+                    <InfoIcon style={{
+                      marginLeft: `${globals.margin}px`
+                    }} />
+                  </Button>
+                </Card.Actions>
+              </Card>
+
+              <Card>
+                <Card.Media alt='App' src={require('../../static/illustrations/app_1.svg')} mode='contain' height={200} />
+                <Card.Title>
+                  We develop native apps.
+                </Card.Title>
+
+                <Card.Content>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id eu nisl nunc mi ipsum faucibus.
+                </Card.Content>
+
+                <Card.Actions>
+                  <Button>
+                    Learn More
+                    <InfoIcon style={{
+                      marginLeft: `${globals.margin}px`
+                    }} />
+                  </Button>
+                </Card.Actions>
+              </Card>
+            </div>
+            
+          </div>
         </Page.Section>
 
         <Page.Section navbarMode='light'>
