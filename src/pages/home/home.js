1602649d5d32a8b9fb8d6434da96336707b82cfb
I_Am_Stephan <stephanburger54@gmail.com>
2019-12-31 16:35:08 +0200

    changed how we do it and used different images

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -235,7 +235,7 @@ export default class Home extends Component {
           <div className={styles['howWeDoIt']}>
             <div className={styles['row']}>
               <div count={1} className={styles['img-container']}>
-                <img alt='We Conceptualize.' src={require('../../static/illustrations/we_conceptualize.svg')} />
+                <img alt='We conceptualize.' src={require('../../static/illustrations/we_conceptualize.svg')} />
               </div>
               <div className={styles['content']}>
                 <h2 className={styles['heading']}>
@@ -246,13 +246,19 @@ export default class Home extends Component {
                     .
                   </span>
                 </h2>
-                 asud hasui dhasi ioa jdoaisj doai jdoiaj sdoiasj oais oiasj doais jdoias doiad joais aois jdoais jdaois doias joiasj d
+                  You contact us and we start the journey. Just pitch us your vision or idea and we will start to conceptualize all the essentials.
               </div>
             </div>
 
-            <div className={styles['row']}>
+            <div className={styles['row-spacer']}>
+              <div className={styles['dot']} />
+              <div className={styles['line']} />
+              <div className={styles['dot']} />
+            </div>
+
+            <div className={`${styles['row']} ${styles['reverse']}`}>
               <div count={2} className={styles['img-container']}>
-                <img alt='We Conceptualize.' src={require('../../static/illustrations/we_design.svg')} />
+                <img alt='We design.' src={require('../../static/illustrations/we_design.svg')} />
               </div>
               
               <div className={styles['content']}>
@@ -264,8 +270,14 @@ export default class Home extends Component {
                     .
                   </span>
                 </h2>
-                 asud hasui dhasi ioa jdoaisj doai jdoiaj sdoiasj oais oiasj doais jdoias doiad joais aois jdoais jdaois doias joiasj d
+                 Just before we start developing we give you design options based on your vision. You can then decide which one best fits your business.
               </div>
+            </div>
+
+            <div className={styles['row-spacer']}>
+              <div className={styles['dot']} />
+              <div className={styles['line']} />
+              <div className={styles['dot']} />
             </div>
 
             <div className={styles['row']}>
@@ -282,11 +294,17 @@ export default class Home extends Component {
                     .
                   </span>
                 </h2>
-                 asud hasui dhasi ioa jdoaisj doai jdoiaj sdoiasj oais oiasj doais jdoias doiad joais aois jdoais jdaois doias joiasj d
+                 Your product is now in development. We will send you iterations of the product to allow you to track our progress.
               </div>
             </div>
 
-            <div className={styles['row']}>
+            <div className={styles['row-spacer']}>
+              <div className={styles['dot']} />
+              <div className={styles['line']} />
+              <div className={styles['dot']} />
+            </div>
+
+            <div className={`${styles['row']} ${styles['reverse']}`}>
               <div count={4} className={styles['img-container']}>
                 <img alt='We Conceptualize.' src={require('../../static/illustrations/we_test.svg')} />
               </div>
@@ -300,8 +318,14 @@ export default class Home extends Component {
                     .
                   </span>
                 </h2>
-                 asud hasui dhasi ioa jdoaisj doai jdoiaj sdoiasj oais oiasj doais jdoias doiad joais aois jdoais jdaois doias joiasj d
+                 Testing is essential to any technological product. We test against security measures and make sure your user experience is at its best.
               </div>
+            </div>
+
+            <div className={styles['row-spacer']}>
+              <div className={styles['dot']} />
+              <div className={styles['line']} />
+              <div className={styles['dot']} />
             </div>
 
             <div className={styles['row']}>
@@ -318,11 +342,17 @@ export default class Home extends Component {
                     .
                   </span>
                 </h2>
-                 asud hasui dhasi ioa jdoaisj doai jdoiaj sdoiasj oais oiasj doais jdoias doiad joais aois jdoais jdaois doias joiasj d
+                 After all our tests are done we finalize the product by adding or removing features to ensure your product is the best there can be.
               </div>
             </div>
 
-            <div className={styles['row']}>
+            <div className={styles['row-spacer']}>
+              <div className={styles['dot']} />
+              <div className={styles['line']} />
+              <div className={styles['dot']} />
+            </div>
+
+            <div className={`${styles['row']} ${styles['reverse']}`}>
               <div count={6} className={styles['img-container']}>
                 <img alt='We Conceptualize.' src={require('../../static/illustrations/we_release.svg')} />
               </div>
@@ -336,7 +366,7 @@ export default class Home extends Component {
                     .
                   </span>
                 </h2>
-                 asud hasui dhasi ioa jdoaisj doai jdoiaj sdoiasj oais oiasj doais jdoias doiad joais aois jdoais jdaois doias joiasj d
+                 Your product is now ready. Your customers can now use your product and simply be amazed at what your business has to offer.
               </div>
             </div>
           </div>
