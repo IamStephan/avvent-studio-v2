d5d59f2de12209e392430f4fea9c4914caad9a19
I_Am_Stephan <stephanburger54@gmail.com>
2020-01-03 21:37:03 +0200

    started work on social media footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,26 +5,31 @@ import styles from './footer.module.scss';
 
 import CopyrightIcon from '@material-ui/icons/Copyright';
 import ArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+// import TwitterIcon from '@material-ui/icons/Twitter';
+// import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import Button from '../button/button';
 
 export default class Footer extends Component {
   render() {
     return (
       <footer className={styles['footer']}>
-        <section>
-          <Button variant='ghost'>
-            <CopyrightIcon style={{
-              marginRight: `${globals.margin}px`
-            }} />
-            Avvent Studio 2020
-          </Button>
-        </section>
+        <div className={styles['footer-bottom']}>
+          <section>
+            <Button variant='ghost'>
+              <CopyrightIcon style={{
+                marginRight: `${globals.margin}px`
+              }} />
+              Avvent Studio 2020
+            </Button>
+          </section>
 
-        <section>
-          <Button onClick={() => window.scroll({top: 0, behavior: 'smooth'})} shape='round'>
-            <ArrowUpIcon />
-          </Button>
-        </section>
+          <section>
+            <Button onClick={() => window.scroll({top: 0, behavior: 'smooth'})} shape='round'>
+              <ArrowUpIcon />
+            </Button>
+          </section>
+        </div>
       </footer>
     );
   }
